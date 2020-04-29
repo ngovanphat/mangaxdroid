6d60d82880e4a1b23556a345979760adca11eef2
ngovanphat <1753082@student.hcmus.edu.vn>
2020-04-29 10:53:10 +0700

    Add UI Home Page

--- app/src/main/java/com/example/mangaxdroid/MainActivity.java
+++ app/src/main/java/com/example/mangaxdroid/MainActivity.java
@@ -2,14 +2,42 @@ package com.example.mangaxdroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.Toast;
+import android.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+import com.synnapps.carouselview.CarouselView;
+import com.synnapps.carouselview.ImageListener;
+
+public class MainActivity extends Activity {
+
+
+
+    CarouselView carouselView;
+    int[] sampleImages = {R.drawable.image_1, R.drawable.image_2, R.drawable.image_3, R.drawable.image_4, R.drawable.image_5};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        carouselView = findViewById(R.id.carouselView);
+        carouselView.setPageCount(sampleImages.length);
+        carouselView.setImageListener(imageListener);
+
     }
+
+    ImageListener imageListener = new ImageListener() {
+        @Override
+        public void setImageForPosition(int position, ImageView imageView) {
+            imageView.setImageResource(sampleImages[position]);
+            Toast.makeText(MainActivity.this,"Ảnh thứ "+position,Toast.LENGTH_SHORT).show();
+        }
+    };
+
 
 }
