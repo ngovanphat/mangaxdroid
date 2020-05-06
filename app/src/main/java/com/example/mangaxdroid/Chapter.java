112ae12d9fb5bf0e23d035247a031d6897ea2ff4
ttnam99 <51222429+ttnam99@users.noreply.github.com>
2020-05-06 22:42:27 +0700

    Revert "Read Chapter Feature - Commit #02"
    This reverts commit e4f3f3e2d4dd622739b123dbde903b3fe67e796a.

--- app/src/main/java/com/example/mangaxdroid/Chapter.java
+++ app/src/main/java/com/example/mangaxdroid/Chapter.java
@@ -1,33 +1,6 @@
 package com.example.mangaxdroid;
 
-import java.util.ArrayList;
-
 public class Chapter {
     private String name;
-    private String id;//Chapter "15.5","Bonus","1"
-    private ArrayList<String> pagesURL;
-
-    public ArrayList<String> getPagesURL() {
-        return pagesURL;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-    public void setPagesURL(ArrayList<String> pagesURL) {
-        this.pagesURL = pagesURL;
-    }
+    private int id;
 }
-
--- app/src/main/java/com/example/mangaxdroid/ReadChapterActivity.java
+++ app/src/main/java/com/example/mangaxdroid/ReadChapterActivity.java
@@ -1,151 +1,85 @@
 package com.example.mangaxdroid;
 
-import android.net.Uri;
+import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
-import android.widget.AbsListView;
+import android.view.ViewConfiguration;
+import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
-
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.widget.Toolbar;
-import androidx.appcompat.app.AppCompatActivity;
-
-
-import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
+import java.util.ArrayList;
 
-import java.net.URL;
-import java.util.List;
-
-public class ReadChapterActivity extends AppCompatActivity {
+public class ReadChapterActivity extends Activity {
     ListView listView;
+    TextView textView;
     ChapterAdapter chapterAdapter;
     BottomNavigationView bottomNav;
     RelativeLayout layout;
-    Toolbar toolbar;
-    ActionBar actionBar;
     int[] images= {R.drawable.pg1, R.drawable.pg2, R.drawable.pg3};
-    List<String> imgURLs;
-    private DatabaseReference dbRef;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_read_chapter);
         listView=findViewById(R.id.imgList);
-        listView.setAdapter(new ChapterAdapter(this, R.layout.chapter_item, images));
+        listView.setAdapter(new ChapterAdapter(this,R.layout.chapter_item,images));
+
         bottomNav=findViewById(R.id.navBar);
         layout = (RelativeLayout) findViewById(R.id.baseLayout);
 
-        toolbar = (Toolbar) findViewById(R.id.toolBar);
-        setSupportActionBar(toolbar);
-        actionBar=getSupportActionBar();
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
-        getSupportActionBar().setTitle("Chapter x: abc");
-
-        listView.setOnScrollListener(new AbsListView.OnScrollListener(){
-            private int lastFirstVisibleItem;
+        layout.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onScrollStateChanged(AbsListView view, int scrollState) {
-            }
-            @Override
-            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
-                if (lastFirstVisibleItem < firstVisibleItem) {
-                    Toast.makeText(getApplicationContext(), "Scrolling down the listView",
-                            Toast.LENGTH_SHORT).show();
-                    bottomNav.setVisibility(View.GONE);
-                    getSupportActionBar().hide();
-                }
-                if (lastFirstVisibleItem > firstVisibleItem) {
-                    Toast.makeText(getApplicationContext(), "Scrolling up the listView",
-                            Toast.LENGTH_SHORT).show();
-                    bottomNav.setVisibility(View.GONE);
-                    getSupportActionBar().hide();
-                }
-                lastFirstVisibleItem = firstVisibleItem;
+            public void onClick(View v) {
+                bottomNav.setVisibility(View.GONE);
             }
         });
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, View view,
-                                    int position, long id) {
-                if(bottomNav.getVisibility()==View.GONE)
-                {
-                    bottomNav.setVisibility(View.VISIBLE);
-                    getSupportActionBar().show();
-                }else {
-                    bottomNav.setVisibility(View.GONE);
-                    getSupportActionBar().hide();
-                }
-            }
-        });
-
-
-
 
         bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(MenuItem item) {
                 switch (item.getItemId()) {
-                    case R.id.action_setting:
-                        //TODO show setting menu
-                        Toast.makeText(ReadChapterActivity.this, "setting", Toast.LENGTH_SHORT).show();
+                    case R.id.action_recents:
+                        Toast.makeText(ReadChapterActivity.this, "Recents", Toast.LENGTH_SHORT).show();
                         break;
-                    case R.id.action_chapterList:
-                        //TODO show chapter list only(no manga info)
+                    case R.id.action_favorites:
                         Toast.makeText(ReadChapterActivity.this, "Favorites", Toast.LENGTH_SHORT).show();
                         break;
-                    case R.id.action_bookmark:
-                        //TODO bookmark then show toast of success
+                    case R.id.action_nearby:
                         Toast.makeText(ReadChapterActivity.this, "Nearby", Toast.LENGTH_SHORT).show();
                         break;                }
                 return true;
             }
         });
     }
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // handle arrow click here
-        if (item.getItemId() == android.R.id.home) {
-            finish(); // close this activity and return to preview activity (if there is any)
-        }
 
-        return super.onOptionsItemSelected(item);
+    public void fetchChapter(String id){
+    //TODO fetch from server
     }
-    public void fetchChapter(String mangaId, final String chapterId){
-    //TODO try running
-        dbRef= FirebaseDatabase.getInstance().getReference("Mangas/"+mangaId+"/Chapters/"+chapterId);
-        dbRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                Chapter chapter = dataSnapshot.getValue(Chapter.class);
-                StorageReference storageRef = FirebaseStorage.getInstance().getReference("chapters/" + chapterId);
-                for (int i = 0; i < chapter.getPagesURL().size(); i++) {
-                    String url=storageRef.child(chapter.getPagesURL().get(i)).getDownloadUrl().toString();
-                    imgURLs.add(url);//URLs cho adapter truyền ảnh vào ImageViews
-                }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {//TODO finish hiding bottom navigation bar
+        super.onTouchEvent(event);
+        long startClickTime=0;
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            startClickTime = System.currentTimeMillis();
+        }
+        else if (event.getAction() == MotionEvent.ACTION_UP) {
+            if (System.currentTimeMillis() - startClickTime < ViewConfiguration.getTapTimeout()) {
             }
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-                System.out.println("The read failed: " + databaseError.getCode());
+            else {
+                bottomNav.setVisibility(View.GONE);
             }
-        });
-    }
-    //Temp function for adding pictures with unique names
-    public void uploadToServer(String mangaId,String chapterID){
-        //TODO upload from res to create storage folders
-        //upload drawables for now
-        //set urls of images to each chapter
-        //
+        }
+        return true;
     }
 }
