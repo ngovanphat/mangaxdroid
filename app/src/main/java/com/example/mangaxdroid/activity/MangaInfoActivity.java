2ef4dd13a93c46ebc0e270fccfab2ee06ec7e998
lmquannnn <leminhquan21199@gmail.com>
2020-05-19 21:44:25 +0700

    Back to previous from Manga Info

--- app/src/main/java/com/example/mangaxdroid/activity/MangaInfoActivity.java
+++ app/src/main/java/com/example/mangaxdroid/activity/MangaInfoActivity.java
@@ -1,92 +1,3 @@
-//package com.example.mangaxdroid.activity;
-//
-//import android.app.Activity;
-//import android.content.Intent;
-//import android.os.Bundle;
-//import android.view.View;
-//import android.widget.AdapterView;
-//import android.widget.ImageView;
-//import android.widget.ListView;
-//import android.widget.TextView;
-//import com.bumptech.glide.Glide;
-//import com.example.mangaxdroid.object.Chapter;
-//import com.example.mangaxdroid.adapter.CustomChapterListAdapter;
-//import com.example.mangaxdroid.R;
-//import com.example.mangaxdroid.object.Manga;
-//import java.util.ArrayList;
-//
-//public class MangaInfoActivity extends Activity {
-//    ImageView managaInfoMainLogo;
-//    ListView listViewChapter;
-//    ArrayList<Chapter> listChapter;
-//    TextView author;
-//    TextView status;
-//    TextView category;
-//    TextView viewCount;
-//    TextView description;
-//    ImageView image;
-//    TextView name;
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.manga_info);
-//        connectContent();
-//
-//        listViewChapter = (ListView) findViewById(R.id.listViewChapter);
-//        listViewChapter.setAdapter(new CustomChapterListAdapter(this, R.layout.chapter_list_custom_row, listChapter));
-//        listViewChapter.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//            @Override
-//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//                Intent intent = new Intent(MangaInfoActivity.this, ReadChapterActivity.class);
-//                startActivity(intent);
-//            }
-//        });
-//
-//        managaInfoMainLogo.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                startActivity(new Intent(MangaInfoActivity.this, MainActivity.class));
-//            }
-//        });
-//
-//        Intent intent = getIntent();
-//        Bundle bundle = intent.getExtras();
-//        Manga manga = (Manga) bundle.getSerializable("manga");
-//
-//        name.setText(manga.getName());
-//        author.setText(manga.getAuthor());
-//        category.setText(manga.getCategory());
-//        viewCount.setText(String.valueOf(manga.getViewCount()));
-//        description.setText(manga.getDescription());
-//        Glide.with(this)
-//                .load(manga.getImage())
-//                .centerCrop()
-//                .into(image);
-//    }
-//
-//    public void readFromStart(View view) {
-//        Intent intent = new Intent(MangaInfoActivity.this, ReadChapterActivity.class);
-//        startActivity(intent);
-//    }
-//
-//    private void connectContent() {
-//        managaInfoMainLogo = (ImageView) findViewById(R.id.managaInfoMainLogo);
-//        author = (TextView) findViewById(R.id.authorName);
-//        name =(TextView) findViewById(R.id.mangaTitle);
-//        category = (TextView) findViewById(R.id.genres);
-//        viewCount = (TextView) findViewById(R.id.views);
-//        description = (TextView) findViewById(R.id.textviewContent);
-//        image = (ImageView) findViewById(R.id.mangaThumbnail);
-//        status = (TextView) findViewById(R.id.status);
-//        listChapter = new ArrayList<>();
-//        listChapter.add(new Chapter("Chapter 1", "01/01/2020", "123456"));
-//        listChapter.add(new Chapter("Chapter 2", "01/01/2020", "123456"));
-//        listChapter.add(new Chapter("Chapter 3", "01/01/2020", "123456"));
-//        listChapter.add(new Chapter("Chapter 4", "01/01/2020", "123456"));
-//        listChapter.add(new Chapter("Chapter 5", "01/01/2020", "123456"));
-//    }
-//}
-
 package com.example.mangaxdroid.activity;
 
 import android.app.FragmentTransaction;
@@ -97,10 +8,10 @@ import android.view.MenuItem;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.fragment.app.FragmentActivity;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import com.bumptech.glide.Glide;
 import com.example.mangaxdroid.R;
 import com.example.mangaxdroid.activity.useractivity.UserFavoriteListActivity;
@@ -108,18 +19,25 @@ import com.example.mangaxdroid.fragment.MangaInfoFragment;
 import com.example.mangaxdroid.object.Manga;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MangaInfoActivity extends FragmentActivity {
+public class MangaInfoActivity extends AppCompatActivity {
     ImageView image;
     TextView name;
     FrameLayout frame;
     Context context;
     BottomNavigationView navigationBarMangaInfo;
+    Toolbar toolbar;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.manga_info_new);
         context = this.getApplicationContext();
         connectContent();
+
+        toolbar = findViewById(R.id.toolBarMangaInfo);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("");
 
         Intent intent = getIntent();
         Bundle bundle = intent.getExtras();
@@ -155,6 +73,14 @@ public class MangaInfoActivity extends FragmentActivity {
                 return true;
             }
         });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private void connectContent() {
