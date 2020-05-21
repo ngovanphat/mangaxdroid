ab682459b2ef4c9265eccf4f9f717b6105c7a1a9
lmquannnn <leminhquan2119@gmail.com>
2020-05-21 09:15:38 +0700

    Load suggested image dinamically

--- app/src/main/java/com/example/mangaxdroid/fragment/HomeFragment.java
+++ app/src/main/java/com/example/mangaxdroid/fragment/HomeFragment.java
@@ -11,11 +11,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.FragmentActivity;
-
-import com.bumptech.glide.Glide;
 import com.example.mangaxdroid.R;
 import com.example.mangaxdroid.object.Manga;
 import com.google.firebase.database.DataSnapshot;
@@ -26,7 +25,6 @@ import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 import com.synnapps.carouselview.CarouselView;
 import com.synnapps.carouselview.ImageListener;
-
 import java.util.ArrayList;
 
 public class HomeFragment extends Fragment {
@@ -34,6 +32,9 @@ public class HomeFragment extends Fragment {
     CarouselView carouselView;
     Button btnViewMore;
     ArrayList<String> imgResource = new ArrayList<>();
+    ArrayList<Manga> mangaArrayList = new ArrayList<>();
+    ImageView suggestedImage1, suggestedImage2, suggestedImage3, suggestedImage4;
+    TextView suggestedText1, suggestedText2, suggestedText3, suggestedText4;
     public static HomeFragment newInstance(){
         HomeFragment fragment = new HomeFragment();
         return fragment;
@@ -55,6 +56,7 @@ public class HomeFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
         carouselView = view.findViewById(R.id.carouselView);
         loadImgResource();
+        loadImgSuggested();
         final Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
@@ -64,6 +66,16 @@ public class HomeFragment extends Fragment {
                 carouselView.setPageCount(imgResource.size());
             }
         }, 5000);
+
+        suggestedImage1 = (ImageView) view.findViewById(R.id.suggestedImage1);
+        suggestedImage2 = (ImageView) view.findViewById(R.id.suggestedImage2);
+        suggestedImage3 = (ImageView) view.findViewById(R.id.suggestedImage3);
+        suggestedImage4 = (ImageView) view.findViewById(R.id.suggestedImage4);
+        suggestedText1 = (TextView) view.findViewById(R.id.suggestedText1);
+        suggestedText2 = (TextView) view.findViewById(R.id.suggestedText2);
+        suggestedText3 = (TextView) view.findViewById(R.id.suggestedText3);
+        suggestedText4 = (TextView) view.findViewById(R.id.suggestedText4);
+
         btnViewMore = (Button) view.findViewById(R.id.btnViewMore);
         btnViewMore.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -75,6 +87,7 @@ public class HomeFragment extends Fragment {
         });
         return view;
     }
+
     private void loadImgResource() {
         DatabaseReference myRef = FirebaseDatabase.getInstance().getReference("Data/ImageResource/carouselView");
         myRef.addValueEventListener(new ValueEventListener() {
@@ -93,6 +106,32 @@ public class HomeFragment extends Fragment {
             }
         });
 
+    }
+
+    private void loadImgSuggested() {
+        DatabaseReference myRef = FirebaseDatabase.getInstance().getReference("Data/Mangas/NewCategory");
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                mangaArrayList.clear();
+                for (DataSnapshot children : dataSnapshot.getChildren()) {
+                    Manga manga = children.getValue(Manga.class);
+                    mangaArrayList.add(manga);
+                }
+                Picasso.get().load(mangaArrayList.get(0).getImage()).into(suggestedImage1);
+                suggestedText1.setText(mangaArrayList.get(0).getName());
+                Picasso.get().load(mangaArrayList.get(1).getImage()).into(suggestedImage2);
+                suggestedText2.setText(mangaArrayList.get(1).getName());
+                Picasso.get().load(mangaArrayList.get(2).getImage()).into(suggestedImage3);
+                suggestedText3.setText(mangaArrayList.get(2).getName());
+                Picasso.get().load(mangaArrayList.get(3).getImage()).into(suggestedImage4);
+                suggestedText4.setText(mangaArrayList.get(3).getName());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+            }
+        });
     }
 
     ImageListener imageListener = new ImageListener() {
