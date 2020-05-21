13df290e50b730789abd036f32c7899b613fd8b9
ngovanphat <1753082@student.hcmus.edu.vn>
2020-05-21 18:05:06 +0700

    add Login activity

---
+++ app/src/main/java/com/example/mangaxdroid/activity/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.mangaxdroid.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mangaxdroid.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
--- app/src/main/java/com/example/mangaxdroid/fragment/UserDefaultFragment.java
+++ app/src/main/java/com/example/mangaxdroid/fragment/UserDefaultFragment.java
@@ -8,13 +8,16 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import androidx.fragment.app.FragmentActivity;
 import com.example.mangaxdroid.R;
+import com.example.mangaxdroid.activity.LoginActivity;
 import com.example.mangaxdroid.activity.useractivity.UserFavoriteListActivity;
 
 public class UserDefaultFragment extends Fragment {
     private FragmentActivity myContext;
+    LinearLayout buttonLogin;
 
     public static UserDefaultFragment newInstance(){
         UserDefaultFragment fragment = new UserDefaultFragment();
@@ -34,7 +37,13 @@ public class UserDefaultFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View view = inflater.inflate(R.layout.fragment_me, container, false);
-
+        buttonLogin = (LinearLayout) view.findViewById(R.id.buttonLogin);
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(view.getContext(), LoginActivity.class));
+            }
+        });
         return view;
     }
 }
