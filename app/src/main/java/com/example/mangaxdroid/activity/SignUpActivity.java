c8ffedfab473428e5d66202281222bfcabb4747a
ngovanphat <1753082@student.hcmus.edu.vn>
2020-05-22 10:21:36 +0700

    login with email and google

--- app/src/main/java/com/example/mangaxdroid/activity/SignUpActivity.java
+++ app/src/main/java/com/example/mangaxdroid/activity/SignUpActivity.java
@@ -13,18 +13,27 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.mangaxdroid.R;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class SignUpActivity extends Activity {
     private FirebaseAuth mAuth;
     private EditText edtEmail,edtPassword,edtConfirmPassword;
-    private Button btnSignUp,btnSignInWithFacebook,btnSignInWithGoogle;
-
-
+    private Button btnSignUp,btnSignInWithFacebook;
+    private SignInButton btnSignInWithGoogle;
+    private int RC_SIGN_IN = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +48,17 @@ public class SignUpActivity extends Activity {
                 String password = edtPassword.getText().toString().trim();
                 String confirm = edtConfirmPassword.getText().toString().trim();
                 SignUp(email,password,confirm);
+            }
+        });
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        final GoogleSignInClient mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        btnSignInWithGoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SignInWithGoogle(mGoogleSignInClient);
             }
         });
     }
@@ -72,11 +92,57 @@ public class SignUpActivity extends Activity {
             }
         }
     }
+    private void SignInWithGoogle(GoogleSignInClient mGoogleSignInClient){
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+          try{
+              // Google Sign In was successful, authenticate with Firebase
+              GoogleSignInAccount account = task.getResult(ApiException.class);
+              Log.d("success", "firebaseAuthWithGoogle:" + account.getId());
+              firebaseAuthWithGoogle(account.getIdToken());
+          } catch (ApiException e) {
+              // Google Sign In failed, update UI appropriately
+              Log.d("failed", "Google sign in failed", e);
+              // ...
+          }
+        }
+    }
+    private void firebaseAuthWithGoogle(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("success", "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.d("failed",  task.getException().getMessage());
+                            Toast.makeText(SignUpActivity.this, "Authentication Failed.",Toast.LENGTH_LONG).show();
+                            updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
+    }
     private void updateUI(FirebaseUser user){
         if(user==null){
             
         }else{
-            
+            Toast.makeText(SignUpActivity.this,user.getEmail(),Toast.LENGTH_LONG).show();
+
         }
     }
 
@@ -85,6 +151,8 @@ public class SignUpActivity extends Activity {
         super.onStart();
         // Check if user is signed in (non-null) and update UI accordingly.
         FirebaseUser currentUser = mAuth.getCurrentUser();
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+
     }
     private void mapping(){
         edtEmail = (EditText) findViewById(R.id.editEmail);
@@ -92,7 +160,7 @@ public class SignUpActivity extends Activity {
         edtConfirmPassword = (EditText) findViewById(R.id.editConfirm);
         btnSignUp = (Button) findViewById(R.id.buttonSignIn);
         btnSignInWithFacebook= (Button) findViewById(R.id.buttonFacebook);
-        btnSignInWithGoogle = (Button) findViewById(R.id.buttonGoogle);
+        btnSignInWithGoogle = (SignInButton) findViewById(R.id.buttonGoogle);
     }
 
 }
