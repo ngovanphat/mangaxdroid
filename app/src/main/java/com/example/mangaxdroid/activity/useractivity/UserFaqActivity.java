01e5a85fd645b740ae9cefaea2c10638134fb4aa
lmquannnn <leminhquan21199@gmail.com>
2020-05-27 14:59:18 +0700

    Finish User FAQ

--- app/src/main/java/com/example/mangaxdroid/activity/useractivity/UserFaqActivity.java
+++ app/src/main/java/com/example/mangaxdroid/activity/useractivity/UserFaqActivity.java
@@ -1,8 +1,14 @@
 package com.example.mangaxdroid.activity.useractivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import com.example.mangaxdroid.R;
@@ -10,6 +16,7 @@ import com.example.mangaxdroid.R;
 
 public class UserFaqActivity extends AppCompatActivity {
     Toolbar toolbar;
+    TextView question1, question2, question3, question4, question5, question6;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -17,9 +24,53 @@ public class UserFaqActivity extends AppCompatActivity {
         setContentView(R.layout.fragment_me_faq);
 
         toolbar = (Toolbar) findViewById(R.id.toolBarFaq);
+        question1 = (TextView) findViewById(R.id.question1);
+        question2 = (TextView) findViewById(R.id.question2);
+        question3 = (TextView) findViewById(R.id.question3);
+        question4 = (TextView) findViewById(R.id.question4);
+        question5 = (TextView) findViewById(R.id.question5);
+        question6 = (TextView) findViewById(R.id.question6);
+
         toolbar.setTitle("");
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        question1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showNiceDialogBox(question1, R.string.question1);
+            }
+        });
+        question2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showNiceDialogBox(question2, R.string.question2);
+            }
+        });
+        question3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showNiceDialogBox(question3, R.string.question3);
+            }
+        });
+        question4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showNiceDialogBox(question4, R.string.question4);
+            }
+        });
+        question5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showNiceDialogBox(question5, R.string.question5);
+            }
+        });
+        question6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showNiceDialogBox(question6, R.string.question6);
+            }
+        });
     }
 
     @Override
@@ -28,5 +79,28 @@ public class UserFaqActivity extends AppCompatActivity {
             finish();
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void showNiceDialogBox(TextView id, int msg) {
+        if (id == question3) {
+            AlertDialog.Builder myBuilder = new AlertDialog.Builder(this);
+            myBuilder.setIcon(R.drawable.mangaxdroid)
+                    .setTitle(id.getText())
+                    .setMessage(msg)
+                    .setPositiveButton("OK", null)
+                    .setNegativeButton("Xem thêm", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int whichOne) {
+                            showNiceDialogBox(question4, R.string.question4);
+                        }})
+                    .show();
+        }
+        else {
+            AlertDialog.Builder myBuilder = new AlertDialog.Builder(this);
+            myBuilder.setIcon(R.drawable.mangaxdroid)
+                    .setTitle(id.getText())
+                    .setMessage(msg)
+                    .setPositiveButton("OK", null)
+                    .show();
+        }
     }
 }
